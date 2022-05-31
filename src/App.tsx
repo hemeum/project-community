aac564cdaab85f11f7f73672f4a7cdccf0a16849
lamenking <dnwjddus1506@hanmail.net>
2022-05-31 21:14:37 +0900

    ✨ Home 라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
+import Home from './pages/Home';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Home></Home>}></Route>
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
