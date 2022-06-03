146673353eda8e6800bbf086858eac2c8242aeba
lamenking <dnwjddus1506@hanmail.net>
2022-06-03 19:42:50 +0900

    🐛 [수정] 로고 컴포넌트 h1 사용

--- src/components/home/Logo.tsx
+++ src/components/home/Logo.tsx
@@ -6,11 +6,13 @@ import logoWebp from './../../assets/logo.webp';
 
 export default function Logo() {
 	return (
-		<Link to="/">
-			<picture>
-				<source srcSet={logoWebp} type="image/webp"></source>
-				<img src={logoPng} alt="넥슨 커뮤니티 로고 이미지"></img>
-			</picture>
-		</Link>
+		<h1>
+			<Link to="/">
+				<picture>
+					<source srcSet={logoWebp} type="image/webp"></source>
+					<img src={logoPng} alt="넥슨 커뮤니티 로고 이미지"></img>
+				</picture>
+			</Link>
+		</h1>
 	);
 }
