ae1a3962a0675ed805a1d702d10a44c3a45b450a
lamenking <dnwjddus1506@hanmail.net>
2022-06-05 00:00:28 +0900

    💄 [추가] 헤더 바텀 - 메뉴 리스트

--- src/components/common/header/HeaderBottom.tsx
+++ src/components/common/header/HeaderBottom.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export interface HeaderBottomProps {
 	menusArr: { id: number; name: string }[];
 }
 
 export default function HeaderBottom({ menusArr }: HeaderBottomProps) {
+	const [isMenu, setIsMenu] = useState('자유게시판');
+
+	const handleIsMenu = (e: any) => {
+		setIsMenu(e.target.textContent);
+	};
+
 	const menus = menusArr.map((menu) => {
-		return <li key={menu.id}>{menu.name}</li>;
+		return (
+			<li
+				className={`text-gray-500 cursor-pointer ${
+					isMenu === menu.name ? 'text-black' : undefined
+				}`}
+				key={menu.id}
+				onClick={handleIsMenu}
+			>
+				{menu.name}
+			</li>
+		);
 	});
 
 	return <ul className="flex gap-8">{menus}</ul>;
