d43c0141a16998d201546edc5efd2941a3f0f13d
lamenking <dnwjddus1506@hanmail.net>
2022-06-04 23:43:31 +0900

    💄 [추가] 헤더 밑 구분선 추가

--- src/layouts/community/CHLayout.tsx
+++ src/layouts/community/CHLayout.tsx
@@ -4,8 +4,11 @@ import CommunityHeader from '../../components/community/CommunityHeader';
 
 export default function CHLayout() {
 	return (
-		<div className="container mx-auto my-12">
-			<CommunityHeader></CommunityHeader>
-		</div>
+		<>
+			<div className="container mx-auto mt-12 mb-6">
+				<CommunityHeader></CommunityHeader>
+			</div>
+			<hr></hr>
+		</>
 	);
 }
