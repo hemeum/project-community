656f6ebe37021f562b7331efdd300c955cf95cc6
lamenking <dnwjddus1506@hanmail.net>
2022-06-05 20:30:31 +0900

    💄 [수정] 변수명 arr > menusArr

--- src/components/community/CommunityHeader.tsx
+++ src/components/community/CommunityHeader.tsx
@@ -9,7 +9,7 @@ export interface CHProps {
 }
 
 export default function CommunityHeader({ isMenu, setIsMenu }: CHProps) {
-	const arr = [
+	const menusArr = [
 		{ id: 1, name: '자유게시판' },
 		{ id: 2, name: '정보게시판' },
 		{ id: 3, name: '토론게시판' },
@@ -18,7 +18,7 @@ export default function CommunityHeader({ isMenu, setIsMenu }: CHProps) {
 	return (
 		<>
 			<HeaderTop category="커뮤니티" />
-			<HeaderBottom menusArr={arr} isMenu={isMenu} setIsMenu={setIsMenu} />
+			<HeaderBottom menusArr={menusArr} isMenu={isMenu} setIsMenu={setIsMenu} />
 		</>
 	);
 }
