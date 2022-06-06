b9cafd0a40ffe418d5e787c9ed088675405f6c24
lamenking <dnwjddus1506@hanmail.net>
2022-06-06 18:02:36 +0900

    ⚡️ 외부영역 클릭 시 닫기

--- src/components/community/CommunitySearch.tsx
+++ src/components/community/CommunitySearch.tsx
@@ -1,30 +1,53 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 export default function CommunitySearch() {
 	const [menuToggle, setMenuToggle] = useState(false);
-	const [optionToggle, setOptionToggle] = useState(false);
+	const [menuOptionToggle, setMenuOptionToggle] = useState(false);
 
-	const handleMenu = () => {
+	const conRef = useRef<HTMLDivElement>(null);
+
+	const openMenu = () => {
 		setMenuToggle(!menuToggle);
-		setOptionToggle(false);
+		setMenuOptionToggle(false);
 	};
 
-	const handleOption = () => {
-		setOptionToggle(!optionToggle);
+	const openMenuOption = () => {
+		setMenuOptionToggle(!menuOptionToggle);
 		setMenuToggle(false);
 	};
 
+	const closeClickOutside = (e: any) => {
+		if (conRef.current !== null) {
+			if (conRef.current && !conRef.current.contains(e.target)) {
+				setMenuToggle(false);
+				setMenuOptionToggle(false);
+			}
+		}
+	};
+
+	useEffect(() => {
+		window.addEventListener('click', closeClickOutside);
+		return () => window.removeEventListener('click', closeClickOutside);
+	}, []);
+
 	return (
-		<div className="flex h-10">
-			<span className="cursor-pointer text-sm" onClick={handleMenu}>
+		<div className="flex h-10" ref={conRef}>
+			<span
+				className="cursor-pointer text-sm"
+				onClick={openMenu}
+				onBlur={() => {
+					setMenuToggle(false);
+					setMenuOptionToggle(false);
+				}}
+			>
 				<div
-					className={`w-32 border border-solid border-box border-gray-300 h-full px-2 flex justify-between items-center`}
+					className={`w-32 border border-solid border-gray-300 h-full px-2 flex justify-between items-center`}
 				>
 					자유게시판
 					<i className="fa-solid fa-angle-down"></i>
 				</div>
 				<ul
-					className={`w-32 px-2 py-3 border border-solid border-t-0 border-box border-gray-300 ${
+					className={`w-32 px-2 py-3 border border-solid border-t-0 border-gray-300 ${
 						menuToggle ? 'opacity-1 visible' : 'opacity-0 invisible'
 					}`}
 				>
@@ -34,16 +57,19 @@ export default function CommunitySearch() {
 					<li className="py-1">토론게시판</li>
 				</ul>
 			</span>
-			<span className="relative cursor-pointer text-sm" onClick={handleOption}>
+			<span
+				className="relative cursor-pointer text-sm"
+				onClick={openMenuOption}
+			>
 				<div
-					className={`w-24 flex justify-between items-center border border-solid  border-box border-r-0 border-l-0 border-gray-300 h-full px-2`}
+					className={`w-24 flex justify-between items-center border border-solid  border-r-0 border-l-0 border-gray-300 h-full px-2`}
 				>
 					제목
 					<i className="fa-solid fa-angle-down ml-8"></i>
 				</div>
 				<ul
-					className={`w-full px-2 py-3 border border-solid border-t-0 border-box border-gray-300 ${
-						optionToggle ? 'opacity-1 visible' : 'opacity-0 invisible'
+					className={`w-24.1 h-28 absolute -left-px -bottom-28 px-2 py-3 border border-solid border-t-0  border-gray-300 ${
+						menuOptionToggle ? 'opacity-1 visible' : 'opacity-0 invisible'
 					}`}
 				>
 					<li className="py-1">제목</li>
